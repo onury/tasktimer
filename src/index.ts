6a76ba0352654b3b21f0ab12139c321cf39f3f5c
Onur Yıldırım <onur@cutepilot.com>
2018-08-12 09:19:00 +0300

    exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './ITaskOptions';
+export * from './ITaskTimerOptions';
+export * from './ITaskTimerEvent';
+export * from './ITimeInfo';
+export * from './Task';
+export * from './TaskCallback';
+export * from './TaskTimer';
