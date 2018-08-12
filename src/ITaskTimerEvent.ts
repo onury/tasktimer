c92fbe514bc78b9255c007568a4500064633aacb
Onur Yıldırım <onur@cutepilot.com>
2018-08-12 09:19:28 +0300

    add ITaskTimerEvent

---
+++ src/ITaskTimerEvent.ts
@@ -0,0 +1,25 @@
+import { TaskTimer } from '.';
+
+/**
+ *  Interface for time information for the latest run of the timer.
+ */
+interface ITaskTimerEvent {
+    /**
+     *  Indicates the type of the event.
+     *  @type {TaskTimer.EventType}
+     */
+    type: TaskTimer.EventType;
+    /**
+     *  Indicates the source object fired this event.
+     *  @type {any}
+     */
+    source: any;
+    /**
+     *  Any object passed to the event emitter. This is generally a `Task`
+     *  instance if set.
+     *  @type {any}
+     */
+    data?: any;
+}
+
+export { ITaskTimerEvent };
