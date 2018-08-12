35d7e4eae07ddb3dff37174807ae7c87553f3a08
Onur Yıldırım <onur@cutepilot.com>
2018-08-12 18:55:47 +0300

    improve tests

--- test/node/tasktimer.spec.ts
+++ test/node/tasktimer.spec.ts
@@ -7,15 +7,13 @@ import { TaskTimer, Task, ITaskOptions, ITaskTimerEvent } from '../../src';
  */
 describe('TaskTimer (Node/TypeScript)', () => {
 
-    const interval = 500;
-
     test('structure', () => {
         expect(TaskTimer.State).toEqual(expect.any(Object));
         expect(TaskTimer.EventType).toEqual(expect.any(Object));
         expect(TaskTimer.Task).toEqual(Task);
     });
 
-    test.only('options & defaults', () => {
+    test('options & defaults', () => {
         let timer = new TaskTimer();
         expect(timer.interval).toEqual(1000);
         expect(timer.stopOnCompleted).toEqual(false);
@@ -41,6 +39,7 @@ describe('TaskTimer (Node/TypeScript)', () => {
         expect(task.tickDelay).toEqual(0);
         expect(task.tickInterval).toEqual(1);
         expect(task.totalRuns).toEqual(null);
+        expect(task.removeOnCompleted).toEqual(false);
 
         task = timer.add({ callback: () => {} }).get('task-1');
         expect(task.enabled).toEqual(true);
@@ -48,6 +47,7 @@ describe('TaskTimer (Node/TypeScript)', () => {
         expect(task.tickDelay).toEqual(0);
         expect(task.tickInterval).toEqual(1);
         expect(task.totalRuns).toEqual(null);
+        expect(task.removeOnCompleted).toEqual(false);
 
         const callback = (o) => o;
         task = new Task({
@@ -56,6 +56,7 @@ describe('TaskTimer (Node/TypeScript)', () => {
             tickDelay: 10,
             tickInterval: 5,
             totalRuns: 3,
+            removeOnCompleted: true,
             callback
         });
         expect(task.enabled).toEqual(false);
@@ -63,11 +64,12 @@ describe('TaskTimer (Node/TypeScript)', () => {
         expect(task.tickDelay).toEqual(10);
         expect(task.tickInterval).toEqual(5);
         expect(task.totalRuns).toEqual(3);
+        expect(task.removeOnCompleted).toEqual(true);
         expect(task.callback).toEqual(callback);
     });
 
     test('timer states', () => {
-        const timer = new TaskTimer(interval);
+        const timer = new TaskTimer(500);
         expect(timer.state).toEqual(TaskTimer.State.IDLE);
         timer.start();
         expect(timer.state).toEqual(TaskTimer.State.RUNNING);
@@ -82,140 +84,225 @@ describe('TaskTimer (Node/TypeScript)', () => {
     });
 
     test('add/remove task', (done) => {
+        const interval = 500;
         const timer = new TaskTimer(interval);
         expect(timer.taskCount).toEqual(0);
 
         const task1Opts: ITaskOptions = {
             name: 'heartbeat',
             tickInterval: 2,
-            totalRuns: 3,
+            totalRuns: 2,
             callback(task: Task) {
-                console.log(task.name + ' task has run ' + task.currentRuns + ' times');
+                console.log(task.name + ' task has run ' + task.currentRuns + ' times @ ', timer.time.elapsed);
                 expect(task.name).toEqual('heartbeat');
+                expect(timer.tickCount % 2).toEqual(0);
                 expect(task.tickInterval).toEqual(task1Opts.tickInterval);
                 expect(task.totalRuns).toEqual(task1Opts.totalRuns);
                 expect(task.currentRuns <= task.totalRuns).toBeTruthy();
                 expect(timer.time.stopped).toEqual(0);
-                let i = task1Opts.tickInterval * task.currentRuns;
-                expect(timer.time.elapsed >= i * interval).toBeTruthy();
-                expect(timer.time.elapsed < (i + 1) * interval).toBeTruthy();
+                let i = task.tickInterval * task.currentRuns;
+                expect(timer.time.elapsed).toBeGreaterThanOrEqual(i * interval);
+                expect(timer.time.elapsed).toBeLessThanOrEqual((i + 1) * interval);
             }
         };
         timer.add(task1Opts);
         expect(timer.taskCount).toEqual(1);
 
         const task2: Task = new Task({
+            name: 'remove-check',
+            tickInterval: 5,
+            totalRuns: 1,
+            removeOnCompleted: true,
+            callback: function (task) {
+                console.log(task.name + ' task has run ' + task.currentRuns + ' times.');
+                expect(timer.tickCount).toEqual(5);
+                expect(timer.get('remove-check')).not.toEqual(null);
+            }
+        });
+        timer.add(task2);
+        expect(timer.taskCount).toEqual(2);
+
+        const task3: ITaskOptions = {
             name: 'final-check',
             tickInterval: 7,
             totalRuns: 1,
             callback: function (task) {
                 console.log(task.name + ' task has run ' + task.currentRuns + ' times.');
                 expect(timer.tickCount).toEqual(7);
+
+                expect(timer.get('remove-check')).toEqual(null);
+                expect(timer.taskCount).toEqual(2);
                 timer.remove('heartbeat');
                 expect(timer.taskCount).toEqual(1);
+
                 timer.stop();
                 expect(timer.state).toEqual(TaskTimer.State.STOPPED);
                 expect(timer.time.stopped).not.toEqual(0);
                 timer.reset();
                 expect(timer.taskCount).toEqual(0);
                 expect(timer.state).toEqual(TaskTimer.State.IDLE);
+
                 done();
             }
-        });
-        timer.add(task2);
-        expect(timer.taskCount).toEqual(2);
+        };
+        timer.add(task3);
+        expect(timer.taskCount).toEqual(3);
+
         timer.start();
     });
 
     test('events', (done) => {
-        const timer = new TaskTimer(interval);
-        const autoTaskName = 'task-1';
+        const timer = new TaskTimer(500);
+        const taskComp: ITaskOptions = {
+            name: 'check-task-completed',
+            totalRuns: 2,
+            callback: () => { }
+        };
+        // second task will be added without name set.
+        // it should be generated as "task-2".
+        const autoTaskName = 'task-2';
+        const executedEvents: TaskTimer.EventType[] = [];
+
         timer
             .reset()
             .on(TaskTimer.EventType.STARTED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.STARTED);
                 console.log('>>> started');
             })
             .on(TaskTimer.EventType.TICK, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.TICK);
-                // expect(event.source).toEqual(timer);
+                expect(event.source).toEqual(timer);
                 expect(event.data).toBeUndefined();
                 console.log('>>> tick', timer.tickCount);
+                if (timer.tickCount === 2) {
+                    timer.add(taskComp);
+                    expect(timer.get(taskComp.name)).not.toBe(null);
+                    expect(timer.taskCount).toEqual(1);
+                }
                 if (timer.tickCount === 3) {
                     timer.add(() => {});
-                    console.log((timer as any)._.tasks);
-                    expect(timer.get(autoTaskName)).toBeDefined();
+                    // console.log((timer as any)._.tasks);
+                    expect(timer.get(autoTaskName)).not.toBe(null);
+                    expect(timer.taskCount).toEqual(2);
                 }
             })
             .on(TaskTimer.EventType.TASK_ADDED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.TASK_ADDED);
-                expect(timer.taskCount).toEqual(1);
                 const task = event.data as Task;
                 expect(task).toBeDefined();
-                if (task) {
-                    console.log('>>> taskAdded', JSON.stringify(task));
+                console.log('>>> taskAdded', JSON.stringify(task));
+                expect(task.tickInterval).toEqual(1);
+                expect(typeof task.callback).toEqual('function');
+                if (task.name === taskComp.name) {
+                    expect(timer.tickCount).toEqual(2);
+                    expect(task.totalRuns).toEqual(taskComp.totalRuns);
+                    expect(timer.taskCount).toEqual(1);
+                } else {
                     expect(task.name).toEqual(autoTaskName);
-                    expect(task.tickInterval).toEqual(1);
-                    expect(task.totalRuns).toEqual(0);
-                    expect(typeof task.callback).toEqual('function');
+                    expect(timer.tickCount).toEqual(3);
+                    expect(task.totalRuns).toEqual(null);
+                    expect(timer.taskCount).toEqual(2);
                 }
             })
             .on(TaskTimer.EventType.TASK, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.TASK);
-                expect(timer.taskCount).toEqual(1);
                 const task = event.data as Task;
                 expect(task).toBeDefined();
-                if (task) {
-                    console.log('>>> task (executed)', JSON.stringify(task));
-                    expect(task.name).toEqual(autoTaskName);
-                    expect(task.tickInterval).toEqual(1);
-                    expect(task.totalRuns).toEqual(0);
-                    expect(typeof task.callback).toEqual('function');
-                }
+                console.log('>>> task (executed)', JSON.stringify(task));
+            })
+            .on(TaskTimer.EventType.TASK_COMPLETED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
+                expect(event.type).toEqual(TaskTimer.EventType.TASK_COMPLETED);
+                expect(timer.taskCount).toEqual(2);
+                const task = event.data as Task;
+                expect(task).toBeDefined();
+                console.log('>>> taskCompleted', JSON.stringify(task));
+                expect(task.name).toEqual(taskComp.name);
                 timer.remove(autoTaskName);
             })
             .on(TaskTimer.EventType.TASK_REMOVED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.TASK_REMOVED);
-                expect(timer.taskCount).toEqual(0);
+                expect(timer.taskCount).toEqual(1);
                 const task = event.data as Task;
                 expect(task).toBeDefined();
-                if (task) {
-                    console.log('>>> taskRemoved', JSON.stringify(task));
-                    expect(task.name).toEqual(autoTaskName);
-                    expect(task.tickInterval).toEqual(1);
-                    expect(task.totalRuns).toEqual(0);
-                    expect(typeof task.callback).toEqual('function');
-                }
+                console.log('>>> taskRemoved', JSON.stringify(task));
+                expect(task.name).toEqual(autoTaskName);
+                expect(timer.get(autoTaskName)).toEqual(null);
                 timer.pause();
             })
             .on(TaskTimer.EventType.PAUSED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.PAUSED);
-                expect(timer.tickCount).toEqual(3);
                 console.log('>>> paused');
                 timer.resume();
             })
             .on(TaskTimer.EventType.RESUMED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.RESUMED);
                 console.log('>>> resumed');
                 timer.stop();
             })
             .on(TaskTimer.EventType.STOPPED, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.STOPPED);
                 console.log('>>> stopped');
                 timer.reset();
             })
+            .on(TaskTimer.EventType.STOPPED, (event: ITaskTimerEvent) => {
+                // second event listener for "stopped"
+                executedEvents.push(event.type);
+            })
             .on(TaskTimer.EventType.RESET, (event: ITaskTimerEvent) => {
+                executedEvents.push(event.type);
                 expect(event.type).toEqual(TaskTimer.EventType.RESET);
                 console.log('>>> reset');
+                checkEvents();
                 done();
             });
+
+        function checkEvents() {
+            expect(executedEvents).toContain(TaskTimer.EventType.STARTED);
+            expect(timer.listenerCount(TaskTimer.EventType.STARTED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.TICK);
+            expect(timer.listenerCount(TaskTimer.EventType.TICK)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.TASK_ADDED);
+            expect(timer.listenerCount(TaskTimer.EventType.TASK_ADDED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.TASK);
+            expect(timer.listenerCount(TaskTimer.EventType.TASK)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.TASK_COMPLETED);
+            expect(timer.listenerCount(TaskTimer.EventType.TASK_COMPLETED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.TASK_REMOVED);
+            expect(timer.listenerCount(TaskTimer.EventType.TASK_REMOVED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.PAUSED);
+            expect(timer.listenerCount(TaskTimer.EventType.PAUSED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.RESUMED);
+            expect(timer.listenerCount(TaskTimer.EventType.RESUMED)).toEqual(1);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.STOPPED);
+            expect(timer.listenerCount(TaskTimer.EventType.STOPPED)).toEqual(2);
+
+            expect(executedEvents).toContain(TaskTimer.EventType.RESET);
+            expect(timer.listenerCount(TaskTimer.EventType.RESET)).toEqual(1);
+        }
+
         timer.start();
         expect(timer.state).toEqual(TaskTimer.State.RUNNING);
         console.log('>>> running');
-        expect(timer.listenerCount(TaskTimer.EventType.RESET)).toEqual(1);
     });
 
-    test.only('task array, stopOnCompleted', (done) => {
+    test('task array, stopOnCompleted', (done) => {
         const timer = new TaskTimer({
             interval: 500,
             stopOnCompleted: true
