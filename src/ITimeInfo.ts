d2cf26ae1c01e65b095b0b4119b1e483813de24a
Onur Yıldırım <onur@cutepilot.com>
2019-01-07 06:24:08 +0300

    update docs

--- src/ITimeInfo.ts
+++ src/ITimeInfo.ts
@@ -1,19 +1,19 @@
 /**
- *  Interface for time information for the latest run of the timer.
+ *  Stores time information for a timer or task.
  */
 interface ITimeInfo {
     /**
-     *  Indicates the start time of the timer.
+     *  Indicates the start time of a timer or task.
      *  @type {number}
      */
     started: number;
     /**
-     *  Indicates the stop time of the timer. (`0` if still running.)
+     *  Indicates the stop time of a timer or task. (`0` if still running.)
      *  @type {number}
      */
     stopped: number;
     /**
-     *  Indicates the the elapsed time of the timer.
+     *  Indicates the the elapsed time of a timer or task, in milliseconds.
      *  @type {number}
      */
     elapsed: number;
