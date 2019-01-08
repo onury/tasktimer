fbfae6a5b75b23de296562f4440017499f45bd7a
Onur Yıldırım <onur@cutepilot.com>
2019-01-08 07:20:42 +0300

    docs

--- src/TaskTimer.ts
+++ src/TaskTimer.ts
@@ -21,7 +21,109 @@ const DEFAULT_TIMER_OPTIONS: ITaskTimerOptions = Object.freeze({
 /**
  *  TaskTimer • https://github.com/onury/tasktimer
  *  @license MIT
- *  @copyright 2018, Onur Yıldırım <onur@cutepilot.com>
+ *  @copyright 2019, Onur Yıldırım <onur@cutepilot.com>
+ */
+
+// ---------------------------
+// EventEmitter Docs
+// ---------------------------
+
+/**
+ *  Calls each of the listeners registered for a given event name.
+ *  @name TaskTimer#emit
+ *  @function
+ *
+ *  @param {TaskTimer.Event} eventName - The name of the event to be emitted.
+ *  @param {any} [data] - Data to be passed to event listeners.
+ *
+ *  @returns {Boolean} - `true` if the event had listeners, else `false`.
+ */
+
+ /**
+  *  Return an array listing the events for which the emitter has registered
+  *  listeners.
+  *  @name TaskTimer#eventNames
+  *  @function
+  *
+  *  @returns {Array} -
+  */
+
+/**
+ *  Adds the listener function to the end of the listeners array for the event
+ *  named `eventName`. No checks are made to see if the listener has already
+ *  been added. Multiple calls passing the same combination of `eventName` and
+ *  `listener` will result in the listener being added, and called, multiple
+ *  times.
+ *  @name TaskTimer#on
+ *  @function
+ *  @chainable
+ *
+ *  @param {TaskTimer.Event} eventName - The name of the event to be added.
+ *  @param {Function} listener - The callback function to be invoked per event.
+ *  @param {*} [context=this] - The context to invoke the listener with.
+ *
+ *  @returns {TaskTimer} - `{@link #TaskTimer|TaskTimer}` instance.
+ */
+
+/**
+ *  Adds a one time listener function for the event named `eventName`. The next
+ *  time `eventName` is triggered, this `listener` is removed and then invoked.
+ *  @name TaskTimer#once
+ *  @function
+ *  @chainable
+ *
+ *  @param {TaskTimer.Event} eventName - The name of the event to be added.
+ *  @param {Function} listener - The callback function to be invoked per event.
+ *  @param {*} [context=this] - The context to invoke the listener with.
+ *
+ *  @returns {TaskTimer} - `{@link #TaskTimer|TaskTimer}` instance.
+ */
+
+/**
+ *  Removes the specified `listener` from the listener array for the event
+ *  named `eventName`.
+ *  @name TaskTimer#off
+ *  @function
+ *  @alias TaskTimer#removeListener
+ *  @chainable
+ *
+ *  @param {TaskTimer.Event} eventName - The name of the event to be removed.
+ *  @param {Function} listener - The callback function to be invoked per event.
+ *  @param {*} [context=this] - Only remove the listeners that have this context.
+ *  @param {Boolean} [once=false] - Only remove one-time listeners.
+ *
+ *  @returns {TaskTimer} - `{@link #TaskTimer|TaskTimer}` instance.
+ */
+
+ /**
+  *  Gets the number of listeners listening to a given event.
+  *  @name TaskTimer#listenerCount
+  *  @function
+  *
+  *  @param {TaskTimer.Event} eventName - The name of the event.
+  *
+  *  @returns {Number} - The number of listeners.
+  */
+
+ /**
+  *  Gets the listeners registered for a given event.
+  *  @name TaskTimer#listeners
+  *  @function
+  *
+  *  @param {TaskTimer.Event} eventName - The name of the event.
+  *
+  *  @returns {Array} - The registered listeners.
+  */
+
+/**
+ *  Removes all listeners, or those of the specified `eventName`.
+ *  @name TaskTimer#removeAllListeners
+ *  @function
+ *  @chainable
+ *
+ *  @param {TaskTimer.Event} [eventName] - The name of the event to be removed.
+ *
+ *  @returns {TaskTimer} - `{@link #TaskTimer|TaskTimer}` instance.
  */
 
 /**
@@ -32,6 +134,10 @@ const DEFAULT_TIMER_OPTIONS: ITaskTimerOptions = Object.freeze({
  *  This class extends `EventEmitter3` which is an `EventEmitter` implementation
  *  for both Node and browser. Only a small set of its methods are documented in
  *  this documentation. For a complete list, refer to Node.js documentation.
+ *  @class
+ *  @global
+ *
+ *  @extends EventEmitter
  *
  *  @see {@link https://nodejs.org/api/events.html#events_class_eventemitter|EventEmitter}
  */
@@ -138,7 +244,7 @@ class TaskTimer extends EventEmitter {
      *  Since the tasks run on ticks instead of millisecond intervals; this
      *  value operates as the base resolution for all tasks. If you are running
      *  heavy tasks; lower interval requires higher CPU power.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#interval
      *  @type {number}
      */
     get interval(): number {
@@ -153,7 +259,7 @@ class TaskTimer extends EventEmitter {
      *  ticks if it's off due to task load. Note that precision will be as high
      *  as possible but it still can be off by a few milliseconds; depending on
      *  the CPU or the load.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#precision
      *  @type {boolean}
      */
     get precision(): boolean {
@@ -168,7 +274,7 @@ class TaskTimer extends EventEmitter {
      *  are completed. For this to take affect, all added tasks should have
      *  `totalRuns` and/or `stopDate` configured. This option can be set/changed
      *  at any time.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#stopOnCompleted
      *  @type {boolean}
      */
     get stopOnCompleted(): boolean {
@@ -181,7 +287,7 @@ class TaskTimer extends EventEmitter {
     /**
      *  Gets the current state of the timer.
      *  For possible values, see `TaskTimer.State` enumeration.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#state
      *  @type {TaskTimer.State}
      *  @readonly
      */
@@ -194,7 +300,7 @@ class TaskTimer extends EventEmitter {
      *  `#time.started` indicates the start time of the timer.
      *  `#time.stopped` indicates the stop time of the timer. (`0` if still running.)
      *  `#time.elapsed` indicates the elapsed time of the timer.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#time
      *  @type {ITimeInfo}
      *  @readonly
      */
@@ -210,7 +316,7 @@ class TaskTimer extends EventEmitter {
     /**
      *  Gets the current tick count for the latest run of the timer.
      *  This value will be reset to `0` when the timer is stopped or reset.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#tickCount
      *  @type {Number}
      *  @readonly
      */
@@ -221,7 +327,7 @@ class TaskTimer extends EventEmitter {
     /**
      *  Gets the current task count. Tasks remain even after the timer is
      *  stopped. But they will be removed if the timer is reset.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#taskCount
      *  @type {Number}
      *  @readonly
      */
@@ -231,7 +337,7 @@ class TaskTimer extends EventEmitter {
 
     /**
      *  Gets the total number of all task executions (runs).
-     *  @memberof TaskTimer
+     *  @name TaskTimer#taskRunCount
      *  @type {Number}
      *  @readonly
      */
@@ -241,7 +347,7 @@ class TaskTimer extends EventEmitter {
 
     /**
      *  Gets the total number of timer runs, including resumed runs.
-     *  @memberof TaskTimer
+     *  @name TaskTimer#runCount
      *  @type {Number}
      *  @readonly
      */
@@ -312,7 +418,7 @@ class TaskTimer extends EventEmitter {
 
         this._.tasks[id] = null;
         delete this._.tasks[id];
-        this._emit(TaskTimer.EventType.TASK_REMOVED, task);
+        this._emit(TaskTimer.Event.TASK_REMOVED, task);
         return this;
     }
 
@@ -334,7 +440,7 @@ class TaskTimer extends EventEmitter {
         this._.stopTime = 0;
         this._markTime();
         this._.startTime = Date.now();
-        this._emit(TaskTimer.EventType.STARTED);
+        this._emit(TaskTimer.Event.STARTED);
         this._run();
         return this;
     }
@@ -350,7 +456,7 @@ class TaskTimer extends EventEmitter {
         if (this.state !== TaskTimer.State.RUNNING) return this;
         this._stop();
         this._.state = TaskTimer.State.PAUSED;
-        this._emit(TaskTimer.EventType.PAUSED);
+        this._emit(TaskTimer.Event.PAUSED);
         return this;
     }
 
@@ -371,7 +477,7 @@ class TaskTimer extends EventEmitter {
         this._runCount++;
         this._markTime();
         this._.state = TaskTimer.State.RUNNING;
-        this._emit(TaskTimer.EventType.RESUMED);
+        this._emit(TaskTimer.Event.RESUMED);
         this._run();
         return this;
     }
@@ -390,7 +496,7 @@ class TaskTimer extends EventEmitter {
         this._stop();
         this._.stopTime = Date.now();
         this._.state = TaskTimer.State.STOPPED;
-        this._emit(TaskTimer.EventType.STOPPED);
+        this._emit(TaskTimer.Event.STOPPED);
         return this;
     }
 
@@ -405,7 +511,7 @@ class TaskTimer extends EventEmitter {
      */
     reset(): TaskTimer {
         this._reset();
-        this._emit(TaskTimer.EventType.RESET);
+        this._emit(TaskTimer.Event.RESET);
         return this;
     }
 
@@ -416,9 +522,9 @@ class TaskTimer extends EventEmitter {
     /**
      *  @private
      */
-    private _emit(type: TaskTimer.EventType, data?: any): boolean {
+    private _emit(type: TaskTimer.Event, data?: any): boolean {
         const event: ITaskTimerEvent = {
-            type,
+            name: type,
             source: this,
             data
         };
@@ -456,7 +562,7 @@ class TaskTimer extends EventEmitter {
         const task = options instanceof TTask ? options : new TTask(options);
         (task as any)._setTimer(this);
         this._.tasks[task.id] = task;
-        this._emit(TaskTimer.EventType.TASK_ADDED, task);
+        this._emit(TaskTimer.Event.TASK_ADDED, task);
         return this;
     }
 
@@ -504,9 +610,9 @@ class TaskTimer extends EventEmitter {
     // @ts-ignore: TS6133: declared but never read.
     private _taskCompleted(task: TTask): void {
         this._.completedTaskCount++;
-        this._emit(TaskTimer.EventType.TASK_COMPLETED, task);
+        this._emit(TaskTimer.Event.TASK_COMPLETED, task);
         if (this._.completedTaskCount === this.taskCount) {
-            this._emit(TaskTimer.EventType.COMPLETED);
+            this._emit(TaskTimer.Event.COMPLETED);
             if (this.stopOnCompleted) this.stop();
         }
         if (task.removeOnCompleted) this.remove(task);
@@ -525,7 +631,7 @@ class TaskTimer extends EventEmitter {
 
         this._.tickCount++;
         this._.tickCountAfterResume++;
-        this._emit(TaskTimer.EventType.TICK);
+        this._emit(TaskTimer.Event.TICK);
 
         // tslint:disable:forin
         for (id in tasks) {
@@ -536,7 +642,7 @@ class TaskTimer extends EventEmitter {
             // completed.
             (task as any)._run(() => {
                 this._.taskRunCount++;
-                this._emit(TaskTimer.EventType.TASK, task);
+                this._emit(TaskTimer.Event.TASK, task);
             });
         }
 
@@ -618,17 +724,20 @@ class TaskTimer extends EventEmitter {
 
 // tslint:disable:no-namespace
 /* istanbul ignore next */
+/** @private */
 namespace TaskTimer {
 
     /**
      *  Represents the class that holds the configurations and the callback function
      *  required to run a task.
+     *  @name TaskTimer.Task
      *  @class
      */
     export const Task = TTask;
 
     /**
      *  Enumerates `TaskTimer` states.
+     *  @memberof TaskTimer
      *  @enum {String}
      *  @readonly
      */
@@ -637,84 +746,73 @@ namespace TaskTimer {
          *  Indicates that the timer is in `idle` state.
          *  This is the initial state when the `TaskTimer` instance is first created.
          *  Also when an existing timer is reset, it will be `idle`.
-         *  @memberof TaskTimer.State
-         *  @type {Number}
+         *  @type {String}
          */
         IDLE = 'idle',
         /**
          *  Indicates that the timer is in `running` state; such as when the timer is
          *  started or resumed.
-         *  @memberof TaskTimer.State
-         *  @type {Number}
+         *  @type {String}
          */
         RUNNING = 'running',
         /**
          *  Indicates that the timer is in `paused` state.
-         *  @memberof TaskTimer.State
-         *  @type {Number}
+         *  @type {String}
          */
         PAUSED = 'paused',
         /**
          *  Indicates that the timer is in `stopped` state.
-         *  @memberof TaskTimer.State
-         *  @type {Number}
+         *  @type {String}
          */
         STOPPED = 'stopped'
     }
 
     /**
      *  Enumerates the `TaskTimer` event types.
+     *  @memberof TaskTimer
      *  @enum {String}
      *  @readonly
      */
-    export enum EventType {
+    export enum Event {
         /**
          *  Emitted on each tick (interval) of `TaskTimer`.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TICK = 'tick',
         /**
          *  Emitted when the timer is put in `RUNNING` state; such as when the timer is
          *  started.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         STARTED = 'started',
         /**
          *  Emitted when the timer is put in `RUNNING` state; such as when the timer is
          *  resumed.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         RESUMED = 'resumed',
         /**
          *  Emitted when the timer is put in `PAUSED` state.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         PAUSED = 'paused',
         /**
          *  Emitted when the timer is put in `STOPPED` state.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         STOPPED = 'stopped',
         /**
          *  Emitted when the timer is reset.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         RESET = 'reset',
         /**
          *  Emitted when a task is executed.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TASK = 'task',
         /**
          *  Emitted when a task is added to `TaskTimer` instance.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TASK_ADDED = 'taskAdded',
@@ -722,7 +820,6 @@ namespace TaskTimer {
          *  Emitted when a task is removed from `TaskTimer` instance.
          *  Note that this will not be emitted when `.reset()` is called; which
          *  removes all tasks silently.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TASK_REMOVED = 'taskRemoved',
@@ -731,13 +828,11 @@ namespace TaskTimer {
          *  or reached its stopping date/time (if set). Note that this event
          *  will only be fired if the tasks has a `totalRuns` limit or a
          *  `stopDate` value set.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TASK_COMPLETED = 'taskCompleted',
         /**
          *  Emitted when a task produces an error on its execution.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         TASK_ERROR = 'taskError',
@@ -746,7 +841,6 @@ namespace TaskTimer {
          *  or reached their stopping date/time (if set). Note that this event
          *  will only be fired if all tasks have a `totalRuns` limit or a
          *  `stopDate` value set.
-         *  @memberof TaskTimer.Event
          *  @type {String}
          */
         COMPLETED = 'completed'
@@ -758,62 +852,3 @@ namespace TaskTimer {
 // ---------------------------
 
 export { TaskTimer };
-
-// ---------------------------
-// ADDITIONAL DOCUMENTATION
-// ---------------------------
-
-/**
- *  Adds the listener function to the end of the listeners array for the event
- *  named `eventType`. No checks are made to see if the listener has already
- *  been added. Multiple calls passing the same combination of eventType and
- *  listener will result in the listener being added, and called, multiple times.
- *  @name TaskTimer#on
- *  @function
- *  @alias TaskTimer#addListener
- *  @chainable
- *
- *  @param {String} eventType - The type of the event to be added.
- *  @param {Function} listener - The callback function to be invoked per event.
- *
- *  @returns {Object} - `{@link #TaskTimer|TaskTimer}` instance.
- */
-
-/**
- *  Adds a one time listener function for the event named `eventType`. The next
- *  time eventType is triggered, this listener is removed and then invoked.
- *  @name TaskTimer#once
- *  @function
- *  @chainable
- *
- *  @param {String} eventType - The type of the event to be added.
- *  @param {Function} listener - The callback function to be invoked per event.
- *
- *  @returns {Object} - `{@link #TaskTimer|TaskTimer}` instance.
- */
-
- /**
-  *  Removes the specified `listener` from the listener array for the event
-  *  named `eventType`.
-  *  @name TaskTimer#off
-  *  @function
-  *  @alias TaskTimer#removeListener
-  *  @chainable
-  *
-  *  @param {String} eventType - The type of the event to be removed.
-  *  @param {Function} listener - The callback function to be invoked per event.
-  *
-  *  @returns {Object} - `{@link #TaskTimer|TaskTimer}` instance.
-  */
-
- /**
-  *  Removes all listeners, or those of the specified eventType.
-  *  @name TaskTimer#removeAllListeners
-  *  @function
-  *  @chainable
-  *
-  *  @param {String} eventType - The type of the event to be removed.
-  *  @param {Function} listener - The callback function to be invoked per event.
-  *
-  *  @returns {Object} - `{@link #TaskTimer|TaskTimer}` instance.
-  */
