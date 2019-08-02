e3b414b228ed1919c4459ffd613cf8028d83d71c
Onur Yıldırım <onur@cutepilot.com>
2019-08-02 03:43:41 +0300

    update docs [skip ci]

--- lib/TaskTimer.d.ts
+++ lib/TaskTimer.d.ts
@@ -166,7 +166,7 @@ declare class TaskTimer extends EventEmitter {
      *      tickDelay: 20,   // ticks (to wait before first run)
      *      tickInterval: 5, // ticks (interval)
      *      totalRuns: 10,   // times to run
-     *      callback(task) {
+     *      callback(task) { // can also be an async function, returning a promise
      *          console.log(task.id + ' task has run ' + task.currentRuns + ' times.');
      *      }
      *  };
